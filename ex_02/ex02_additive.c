d2d1268be6b5df5b993d87b5b5573ca4ba0812b1
NiccoloNiccolo <niccolo.gambarotto@studenti.unipd.it>
2026-04-23 14:54:40 +0200

    Completed ex02 assignment

--- ex_02/ex02_additive.c
+++ ex_02/ex02_additive.c
@@ -19,8 +19,7 @@ Run:
 Output:
 - ex02_additive.wav
 */
-
-#define _GNU_SOURCE
+#define M_PI 3.14159265358979323846
 #include <math.h>
 #include <stdio.h>
 #include "libs/tinywav/tinywav.h"
@@ -35,35 +34,23 @@ typedef struct Oscillator {
 float oscillator_next(Oscillator *osc)
 {
 	float sample = osc->amplitude * sinf(osc->phase);
+	// 1. Increase `osc->phase` by one step. The step depends on frequency and sample rate.
 	float step = 2 * M_PI * osc->frequency / osc->sample_rate;
 	osc->phase += step;
-	if(osc->phase > 2*M_PI) {osc->phase -= 2*M_PI;}
-  /*
-  CHANGEME:
-  Update the phase of the oscillator after computing the current sample.
-
-  What your code should do:
-  1. Increase `osc->phase` by one step.
-     The step depends on frequency and sample rate.
-  2. The step should be:
-     2 * pi * frequency / sample_rate
-  3. If the phase becomes greater than or equal to one full turn (`2 * pi`),
-     subtract one full turn so that the phase stays in a small range.
-
-  Use the fields inside the struct through the pointer `osc`.
-  */
-
-  return sample;
+	// 3. If the phase becomes greater than or equal to one full turn (`2 * pi`), subtract one full turn so that the phase stays in a small range.
+	if(osc->phase > 2 * M_PI)
+	{ osc->phase -= 2 * M_PI;}
+	return sample;
 }
 
 int main(void)
 {
-  const int sample_rate = 48000;
-  const int num_frames = sample_rate;
-  float samples[480];
-  TinyWav wav;
-  Oscillator oscillators[3];
-  int frame_index;
+	const int sample_rate = 48000;
+	const int num_frames = sample_rate;
+	float samples[480];
+	TinyWav wav;
+	Oscillator oscillators[3];
+	//int frame_index;
 
   /*
   Initialize all three oscillators.
@@ -83,17 +70,52 @@ int main(void)
   - oscillator 1: frequency 440 Hz, amplitude 0.15
   - oscillator 2: frequency 660 Hz, amplitude 0.10
   */
-
-  oscillators[0].phase = 0.0f;
-  oscillators[0].frequency = 220.0f;
-  oscillators[0].amplitude = 0.30f;
-  oscillators[0].sample_rate = (float) sample_rate;
-
-  if (tinywav_open_write(&wav, 1, sample_rate, TW_FLOAT32, TW_INLINE, "ex02_additive.wav") != 0) {
-    fprintf(stderr, "Could not open ex02_additive.wav for writing.\n");
-    return 1;
-  }
-
+	// Oscillator 0
+	oscillators[0].phase = 0.0f;
+	oscillators[0].frequency = 440.0f;
+	oscillators[0].amplitude = 0.30f;
+	oscillators[0].sample_rate = (float) sample_rate;
+	// Oscillator 1
+	oscillators[1].phase = 0.0f;
+	oscillators[1].frequency = 220.0f;
+	oscillators[1].amplitude = 0.15;
+	oscillators[1].sample_rate = (float) sample_rate;
+	// Oscillator 2
+	oscillators[2].phase = 0.0f;
+	oscillators[2].frequency = 660.0f;
+	oscillators[2].amplitude = 0.10;
+	oscillators[2].sample_rate = (float) sample_rate;
+	
+	if (tinywav_open_write(&wav, 1, sample_rate, TW_FLOAT32, TW_INLINE, "ex02_additive.wav") != 0) 
+	{
+		fprintf(stderr, "Could not open ex02_additive.wav for writing.\n");
+		return 1;
+	}
+	
+	for(int i = 0; i < num_frames; i = i + 480)
+	{
+		// 1. Process the full signal in blocks of 480 samples, as in exercise 1.
+     		int block_size;
+     		// 2. Compute the correct block size, especially for the last block.
+     		if((num_frames - i) < 480)
+     		{
+     			block_size = num_frames - i;
+     		}
+		else {block_size = 480;} 
+		
+		/* 3. For each sample in the block:
+		     - start from `sample = 0.0f`
+		     - call `oscillator_next(&oscillators[j])` for each of the three oscillators
+		     - add the three values together
+		     - store the result in `samples[i]`*/
+		for(int j = 0; j < block_size; j++)
+		{
+			samples[j] = oscillator_next(&oscillators[0]) + oscillator_next(&oscillators[1]) + oscillator_next(&oscillators[2]);
+		}
+		
+		//4. After filling a block, write it with `tinywav_write_f(...)`.
+		tinywav_write_f(&wav, samples, block_size);
+	}
   /*
   Write the main synthesis loop here.
 
